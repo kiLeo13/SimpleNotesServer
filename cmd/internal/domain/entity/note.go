50c02d9682b8ba1d7b885f977499fd92baf32edf
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-08-31 15:30:31 -0300

    Remove note `type` field

--- cmd/internal/domain/entity/note.go
+++ cmd/internal/domain/entity/note.go
@@ -3,7 +3,6 @@ package entity
 type Note struct {
 	ID          int    `gorm:"primaryKey"`
 	Name        string `gorm:"not null"`
-	Type        string `gorm:"not null"`
 	CreatedByID int    `gorm:"not null"` // References: users(id)
 	Content     string `gorm:"not null"`
 	IsPrivate   bool   `gorm:"not null"`
