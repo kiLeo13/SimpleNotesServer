e2ce390d45fa3a93f61d4c816457745e4f97eabf
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-09-08 02:29:52 -0300

    Oops

--- cmd/internal/routes/note_routes.go
+++ cmd/internal/routes/note_routes.go
@@ -1,7 +1,6 @@
 package routes
 
 import (
-	"fmt"
 	"github.com/labstack/echo/v4"
 	"mime/multipart"
 	"net/http"
@@ -38,32 +37,25 @@ func (n *DefaultNoteRoute) GetNotes(c echo.Context) error {
 }
 
 func (n *DefaultNoteRoute) CreateNote(c echo.Context) error {
-	fmt.Println("Received")
 	var req service.NoteRequest
 	if err := c.Bind(&req); err != nil {
 		return c.JSON(400, apierror.MalformedBodyError)
 	}
 
-	fmt.Println("Parsing token...")
 	data, err := utils.ParseTokenData(c)
 	if err != nil {
 		return c.JSON(401, apierror.InvalidAuthTokenError)
 	}
-	fmt.Println("Parsed")
 
-	fmt.Println("Getting content")
 	fileHeader, err := c.FormFile("content")
 	if err != nil {
 		return c.JSON(400, apierror.MissingNoteFileError)
 	}
-	fmt.Println("Got content")
 
-	fmt.Println("Calling creator")
 	note, apierr := n.NoteService.CreateNote(&req, fileHeader, data.Sub)
 	if apierr != nil {
 		return c.JSON(apierr.Code(), apierr)
 	}
-	fmt.Println("Got response")
 	return c.JSON(http.StatusCreated, &note)
 }
 
