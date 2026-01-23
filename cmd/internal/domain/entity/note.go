2243ac9a92f08ae9dd6711617f591bdd65ba8762
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-01-23 00:30:37 -0300

    Introduce new `NoteType` type at `note.go`

--- cmd/internal/domain/entity/note.go
+++ cmd/internal/domain/entity/note.go
@@ -1,16 +1,24 @@
 package entity
 
+type NoteType string
+
+const (
+	NoteTypeReference NoteType = "REFERENCE"
+	NoteTypeMarkdown  NoteType = "MARKDOWN"
+	NoteTypeFlowchart NoteType = "FLOWCHART"
+)
+
 type Note struct {
-	ID          int    `gorm:"primaryKey"`
-	Name        string `gorm:"not null"`
-	Content     string `gorm:"not null"`
-	CreatedByID int    `gorm:"not null"` // References: users(id)
-	Tags        string `gorm:"not null"`
-	NoteType    string `gorm:"not null"`
-	ContentSize int    `gorm:"not null"`
-	Visibility  string `gorm:"not null"`
-	CreatedAt   int64  `gorm:"not null"`
-	UpdatedAt   int64  `gorm:"not null;autoUpdateTime:false"`
+	ID          int      `gorm:"primaryKey"`
+	Name        string   `gorm:"not null"`
+	Content     string   `gorm:"not null"`
+	CreatedByID int      `gorm:"not null"` // References: users(id)
+	Tags        string   `gorm:"not null"`
+	NoteType    NoteType `gorm:"not null"`
+	ContentSize int      `gorm:"not null"`
+	Visibility  string   `gorm:"not null"`
+	CreatedAt   int64    `gorm:"not null"`
+	UpdatedAt   int64    `gorm:"not null;autoUpdateTime:false"`
 
 	// Relations
 	CreatedBy User `gorm:"foreignKey:CreatedByID;references:ID"`
