79faab9dfe5fbed38cb2d95d9e7ec1b3e3cdf2db
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-01-23 10:34:03 -0300

    Add `Permissions` column to the `users`  table

--- cmd/internal/domain/entity/user.go
+++ cmd/internal/domain/entity/user.go
@@ -2,12 +2,13 @@ package entity
 
 // User is the general basic structure of all users across the platform
 type User struct {
-	ID            int    `gorm:"primaryKey"`
-	SubUUID       string `gorm:"not null"`
-	Username      string `gorm:"not null"`
-	Email         string `gorm:"not null"`
-	EmailVerified bool   `gorm:"not null"`
-	IsAdmin       bool   `gorm:"not null"`
-	CreatedAt     int64  `gorm:"not null"`
-	UpdatedAt     int64  `gorm:"not null"`
+	ID            int        `gorm:"primaryKey"`
+	SubUUID       string     `gorm:"not null"`
+	Username      string     `gorm:"not null"`
+	Email         string     `gorm:"not null"`
+	EmailVerified bool       `gorm:"not null"`
+	Permissions   Permission `gorm:"not null;type:bigint;default:0"`
+	IsAdmin       bool       `gorm:"not null"`
+	CreatedAt     int64      `gorm:"not null"`
+	UpdatedAt     int64      `gorm:"not null"`
 }
