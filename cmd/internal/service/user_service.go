ce2da02cf280d53bbd98f08da83ff953bfae6470
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-01-24 03:10:16 -0300

    Remove `IsAdmin` field from user responses

--- cmd/internal/service/user_service.go
+++ cmd/internal/service/user_service.go
@@ -64,7 +64,6 @@ type UserResponse struct {
 	ID        int    `json:"id"`
 	Username  string `json:"username"`
 	Perms     int64  `json:"permissions"`
-	IsAdmin   bool   `json:"is_admin"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
 }
@@ -357,7 +356,6 @@ func toUserResponse(user *entity.User) *UserResponse {
 		ID:        user.ID,
 		Username:  user.Username,
 		Perms:     int64(user.Permissions),
-		IsAdmin:   user.Permissions.HasRaw(entity.PermissionAdministrator),
 		CreatedAt: utils.FormatEpoch(user.CreatedAt),
 		UpdatedAt: utils.FormatEpoch(user.UpdatedAt),
 	}
