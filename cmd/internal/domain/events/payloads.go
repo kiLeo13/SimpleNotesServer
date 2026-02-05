55c3dc2fb850058537d427ad04f4389b5c2abedf
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-02-05 10:27:31 -0300

    Add more events payloads

--- cmd/internal/domain/events/payloads.go
+++ cmd/internal/domain/events/payloads.go
@@ -1,9 +1,30 @@
 package events
 
+import "simplenotes/cmd/internal/service"
+
+// CreateNoteEvent holds a whole note response body.
+type CreateNoteEvent struct {
+	*service.NoteResponse
+}
+
+func (e *CreateNoteEvent) GetType() EventType {
+	return EventNoteCreate
+}
+
+// UpdateNoteEvent holds a whole note response body.
+type UpdateNoteEvent struct {
+	*service.NoteResponse
+}
+
+func (e *UpdateNoteEvent) GetType() EventType {
+	return EventNoteUpdate
+}
+
+// DeleteNoteEvent holds only the note ID.
 type DeleteNoteEvent struct {
 	NoteID int `json:"id"`
 }
 
-func (e DeleteNoteEvent) GetType() EventType {
+func (e *DeleteNoteEvent) GetType() EventType {
 	return EventNoteDelete
 }
